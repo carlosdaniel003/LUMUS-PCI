3993180a7884553e8aa34ee268f73b800dd8e5b2
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-08-06 16:28:52 -0400

    test(display): validar ausência de loop no resize

--- tests/test_display_settings_fullscreen.py
+++ tests/test_display_settings_fullscreen.py
@@ -9,6 +9,45 @@ from src.platform.display_settings_fullscreen import (
 from src.ui.main_window import ODINView
 
 
+class FakeRootFrame:
+    def __init__(self):
+        self.pack_calls = []
+
+    def pack_configure(self, **kwargs):
+        self.pack_calls.append(kwargs)
+
+
+class FakeWindow:
+    def __init__(self, width=1920):
+        self.width = width
+        self.frame = FakeRootFrame()
+        self.bindings = {}
+        self.attr_calls = []
+        self.lift_calls = 0
+        self.focus_calls = 0
+
+    def winfo_children(self):
+        return (self.frame,)
+
+    def winfo_width(self):
+        return self.width
+
+    def bind(self, event, callback, add=None):
+        self.bindings[event] = callback
+
+    def overrideredirect(self, _value):
+        return None
+
+    def attributes(self, name, value):
+        self.attr_calls.append((name, value))
+
+    def lift(self):
+        self.lift_calls += 1
+
+    def focus_force(self):
+        self.focus_calls += 1
+
+
 class DisplaySettingsFullscreenTests(unittest.TestCase):
     def test_margem_compacta_em_tela_menor(self):
         self.assertEqual(14, calcular_margem_responsiva(900))
@@ -39,6 +78,33 @@ class DisplaySettingsFullscreenTests(unittest.TestCase):
         codigo = inspect.getsource(modulo)
         self.assertEqual(1, codigo.count("janela.after("))
         self.assertNotIn("for atraso in", codigo)
+
+    def test_fullscreen_e_ativado_somente_uma_vez(self):
+        janela = FakeWindow()
+
+        modulo._ativar_tela_cheia(janela)
+        modulo._ativar_tela_cheia(janela)
+
+        self.assertEqual([("-fullscreen", True)], janela.attr_calls)
+        self.assertEqual(1, janela.lift_calls)
+        self.assertEqual(1, janela.focus_calls)
+
+    def test_resize_igual_nao_dispara_reflow_repetido(self):
+        janela = FakeWindow(width=1920)
+
+        modulo._instalar_margem_responsiva(janela)
+        self.assertEqual(1, len(janela.frame.pack_calls))
+
+        callback = janela.bindings["<Configure>"]
+        evento_full_hd = type("Event", (), {"width": 1920})()
+        callback(evento_full_hd)
+        callback(evento_full_hd)
+
+        self.assertEqual(1, len(janela.frame.pack_calls))
+
+        evento_hd = type("Event", (), {"width": 1280})()
+        callback(evento_hd)
+        self.assertEqual(2, len(janela.frame.pack_calls))
 
     def test_instalador_e_idempotente(self):
         original = ODINView.abrir_janela_configuracoes
